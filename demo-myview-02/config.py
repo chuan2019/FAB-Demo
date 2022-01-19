98843ca0e812336bcbb986f1780a31e2f4964b65
Chuan <47965229+chuan2019@users.noreply.github.com>
2022-01-18 22:58:23 -0800

    Update config.py

--- demo-myview-02/config.py
+++ demo-myview-02/config.py
@@ -1,10 +1,11 @@
+"""config.py"""
 import os
 from flask_appbuilder.security.manager import AUTH_DB
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Your App secret key
-SECRET_KEY = "\2\1thisismyscretkey\1\2\e\y\y\h"
+SECRET_KEY = r"\2\1thisismyscretkey\1\2\e\y\y\h"
 
 # The SQLAlchemy connection string.
 SQLALCHEMY_DATABASE_URI = 'postgresql://dbc:dbc@localhost:5433/fab_demo_02'
