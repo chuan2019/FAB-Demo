dc459387c8b515bb976bbdc1c22aafe80acc7fb7
Chuan <47965229+chuan2019@users.noreply.github.com>
2022-01-18 22:19:41 -0800

    Update models.py

--- demo-myview-01/app/models.py
+++ demo-myview-01/app/models.py
@@ -1,12 +1,4 @@
+"""models.py"""
 from flask_appbuilder import Model
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-
-"""
-
-You can use the extra Flask-AppBuilder fields and Mixin's
-
-AuditMixin will add automatic timestamp of created and modified by who
-
-
-"""
