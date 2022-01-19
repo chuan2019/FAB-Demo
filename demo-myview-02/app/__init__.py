1816202afa2dedaef9eec96b99c3f06a3c99047c
Chuan <47965229+chuan2019@users.noreply.github.com>
2022-01-18 22:35:31 -0800

    Update __init__.py

--- demo-myview-02/app/__init__.py
+++ demo-myview-02/app/__init__.py
@@ -1,32 +1,15 @@
+"""__init__.py"""
 import logging
 
 from flask import Flask
 from flask_appbuilder import AppBuilder, SQLA
 
-"""
- Logging configuration
-"""
-
+# Logging configuration
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
 
+# Creating and configuring flask app
 app = Flask(__name__)
 app.config.from_object("config")
 db = SQLA(app)
 appbuilder = AppBuilder(app, db.session)
-
-
-"""
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
-
-#Only include this for SQLLite constraints
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    # Will force sqllite contraint foreign keys
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
-"""
-
-from . import views
