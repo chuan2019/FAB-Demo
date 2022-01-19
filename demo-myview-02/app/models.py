5e702309a93fcf7c03e4a869b965ee1576b59ea8
Chuan <47965229+chuan2019@users.noreply.github.com>
2022-01-18 22:46:05 -0800

    Update models.py

--- demo-myview-02/app/models.py
+++ demo-myview-02/app/models.py
@@ -1,3 +1,4 @@
+"""models.py"""
 from sqlalchemy import Column, DateTime, Integer, String, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 
@@ -6,15 +7,17 @@ from flask_appbuilder.models.mixins import AuditMixin
 
 
 class Projects(Model):
+    """Projects Table"""
     id           = Column(Integer, primary_key=True)
     project_name = Column(String(50), unique = True, nullable=False)
     description  = Column(String(300), nullable=True)
-    
+
     def __repr__(self):
-        return self.project_name
+        return self.project_name.__str__()
 
 
 class Tasks(Model):
+    """Tasks Table"""
     id          = Column(Integer, primary_key=True)
     task_name   = Column(String(50), unique=True, nullable=False)
     description = Column(String(300), nullable=True)
@@ -22,16 +25,17 @@ class Tasks(Model):
     project_tbl = relationship('Projects', foreign_keys='Tasks.project_id')
 
     def __repr__(self):
-        return self.task_name
-    
+        return self.task_name.__str__()
+
 
 class TaskProgress(AuditMixin, Model):
+    """TaskProgress Table"""
     id          = Column(Integer, primary_key=True)
     task_id     = Column(Integer, ForeignKey(Tasks.id))
     start_time  = Column(DateTime, nullable=False)
     end_time    = Column(DateTime, nullable=True)
-    status      = Column(Enum('QUEUED', 'STARTED', 'FINISHED', 'FAILED', 'PAUSED', 'CANCELED', name='StatusEnum'),
+    status      = Column(Enum('QUEUED', 'STARTED', 'FINISHED', 'FAILED',
+                              'PAUSED', 'CANCELED', name='StatusEnum'),
                          default='STARTED', nullable=False)
     comment     = Column(String(300), nullable=True)
     task_tbl    = relationship('Tasks', foreign_keys='TaskProgress.task_id')
-
