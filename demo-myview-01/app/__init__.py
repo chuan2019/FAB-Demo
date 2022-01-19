36d56bb5adceb4083fcb463102b63d0b81fd1884
Chuan <47965229+chuan2019@users.noreply.github.com>
2022-01-18 22:09:31 -0800

    Update __init__.py

--- demo-myview-01/app/__init__.py
+++ demo-myview-01/app/__init__.py
@@ -1,19 +1,17 @@
+"""__init__.py"""
 import logging
 
 from flask import Flask
 from flask_appbuilder import AppBuilder, SQLA
 
-"""
- Logging configuration
-"""
-
+# Logging configuration
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
 
+# creating and configuring flask app
 app = Flask(__name__)
 app.config.from_object("config")
 db = SQLA(app)
 appbuilder = AppBuilder(app, db.session)
 
-
-from . import views
+from app import views
