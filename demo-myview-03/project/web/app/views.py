068d642fdb9baf29f1865a14354b3412769fb1be
Chuan <47965229+chuan2019@users.noreply.github.com>
2022-01-29 10:46:23 -0800

    Update views.py

--- demo-myview-03/project/web/app/views.py
+++ demo-myview-03/project/web/app/views.py
@@ -7,6 +7,7 @@ from . import appbuilder, db
 from .models import EmailModel
 
 class EmailModelView(ModelView):
+    """ModelView for the EmailModel"""
     datamodel = SQLAInterface(EmailModel)
     list_columns = ['id', 'email_address']
 
@@ -22,12 +23,11 @@ appbuilder.add_view(
 
 # Application wide 404 error handler
 @appbuilder.app.errorhandler(404)
-def page_not_found(e):
+def page_not_found(e): # pylint: disable=C0103,W0613
+    """page when 404 error occurs"""
     return (
         render_template(
             "404.html", base_template=appbuilder.base_template, appbuilder=appbuilder
         ),
         404,
     )
-
-
