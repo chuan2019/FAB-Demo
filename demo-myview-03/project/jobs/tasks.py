a74b6389b507e378f5b1414d9647a10cbbbc4d75
Chuan <47965229+chuan2019@users.noreply.github.com>
2022-01-29 10:25:33 -0800

    Update tasks.py
    fix pylint errors

--- demo-myview-03/project/jobs/tasks.py
+++ demo-myview-03/project/jobs/tasks.py
@@ -5,18 +5,19 @@ import ssl
 from email.mime.text import MIMEText
 from celery.schedules import crontab
 
-from jobs import (
+from jobs import ( # pylint: disable=E0401
     celery_app,
     SMTP_HOST,
     SMTP_PORT,
     SMTP_ACCOUNT,
-    SMTP_PASSWORD
+    SMTP_PASSWORD,
+    logger
 )
 
-from jobs.emails import get_recipients, get_message
+from jobs.emails import get_recipients, get_message # pylint: disable=E0401
 
 @celery_app.task(bind=True)
-def email_notification(self) -> None:
+def email_notification(self) -> None: # pylint: disable=W0613
     '''send email notification'''
     url = os.environ.get('WEB_SERVER_URL', 'http://localhost:8080')
     from_email = SMTP_ACCOUNT
@@ -36,16 +37,15 @@ def email_notification(self) -> None:
                 msg['From']    = from_email
                 msg['To']      = ",".join(to_email)
                 smtp_server.sendmail(from_email, to_email, msg.as_string())
-    except Exception as err:
-        print(str(err))
+    except smtplib.SMTPException as err:
+        logger.error(str(err))
         return False
     return True
 
 @celery_app.on_after_configure.connect
-def send_weekly_notice(sender, **kwargs):
+def send_weekly_notice(sender, **kwargs):  # pylint: disable=W0613
     '''add periodic task into the schedule'''
     sender.add_periodic_task(
         crontab(hour=7, minute='30', day_of_week=1),
         email_notification.s(),
     )
-
